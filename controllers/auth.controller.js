ea49d9aa78507dfa0989919e82656789a1485b95
Felipe Rodriguez Letelier <virusfeli@gmail.com>
2022-08-17 16:34:07 -0300

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,51 @@
-export const register = (req, res) => {
-    res.json({ok: "Register" });
+import { User } from '../models/user.js'
+import jwt from 'jsonwebtoken'
+
+export const register = async(req, res) => {
+    const {email, password} = req.body
+    try {
+        //Second alternative searching by email
+        let user = await User.findOne({ email });
+        if (user) throw { code: 11000};
+
+        user = new User({ email, password });
+        await user.save();
+
+        //Generar el token JWT
+
+        return res.json({ok: true})
+    } catch (error) {
+        console.log(error);
+        //Default alternative mongoose
+        if(error.code === 11000){
+            return res.status(400).json({error: "Ya existe este usuario"})
+        }
+        return res.status(500).json({error: "Server error"});
+    }
+   
 };
 
-export const login = (req, res) => {
-    res.json({ok: "Login" });
+export const login = async(req, res) => {
+    try {
+    const {email, password} = req.body   
+
+    let user = await User.findOne({ email });   
+    if (!user) return res.status(403).json({error: "No existe este usuario"})
+    
+    const respuestaPassword = await user.comparePassword(password)   
+    if (!respuestaPassword) 
+        return res.status(500).json({error: "Contraseña incorrecta"});
+
+    //Generar el token JWT   
+    const token = jwt.sign({uid: user._id}, process.env.JWT_SECRET) 
+
+    return res.json({ok: token });
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({error: "Server error"});
+    }
+
+  
 };
 
--- models/user.js
+++ models/user.js
@@ -1,20 +1,42 @@
-import {Schema, model} from "mongoose";
+import bcryptjs from "bcryptjs";
+import mongoose from "mongoose";
 
-const userSchema = new Schema({
-    email:{
+
+const userSchema = new mongoose.Schema({
+    email: {
         type: String,
         required: true,
         trim: true,
         unique: true,
         lowercase:true,
-        index: {unique: true}
-
-
+        index: {unique: true},
     },
     password:{
         type: String,
         required: true
-    }
-})
+    },
+});
 
-export const User = model("user", userSchema)
+userSchema.pre("save", async function (next) {
+    const user = this;
+    
+    if (!user.isModified("password")) return next();
+
+
+    try {
+        const salt = await bcryptjs.genSalt(10);
+        user.password = await bcryptjs.hash(user.password, salt);
+        next();
+    } catch (error) {
+        console.log(error)
+        throw new Error('Password hash failed');
+    }
+});
+
+
+userSchema.methods.comparePassword = async function(candidatePassword){
+    return await bcryptjs.compare(candidatePassword, this.password);
+}
+
+export const User = mongoose.model("User", userSchema);
+
